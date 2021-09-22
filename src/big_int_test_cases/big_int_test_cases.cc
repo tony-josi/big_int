07b2bb4844273a76b85b001988eb2e4917d9946e
TonyJosi97 <tonyjosinew@gmail.com>
2021-09-23 00:26:43 +0530

    pybind api src

--- src/big_int_test_cases/big_int_test_cases.cc
+++ src/big_int_test_cases/big_int_test_cases.cc
@@ -40,3 +40,25 @@ std::string bi_test_big_int_unsigned_sub(const std::string &a, const std::string
     return test_bi_c.big_int_to_string(bi_base::BI_HEX);
 
 }
+
+std::string bi_test_big_int_unsigned_add_on_obj(const std::string &a, const std::string &b) {
+
+    big_int test_bi_a, test_bi_b;
+    test_bi_a.big_int_from_string(a);
+    test_bi_b.big_int_from_string(b);
+    test_bi_a.big_int_unsigned_add(test_bi_b);
+    return test_bi_a.big_int_to_string(bi_base::BI_HEX);
+
+}
+
+/*
+std::string bi_test_big_int_unsigned_sub_on_obj(const std::string &a, const std::string &b) {
+
+    big_int test_bi_a, test_bi_b;
+    test_bi_a.big_int_from_string(a);
+    test_bi_b.big_int_from_string(b);
+    test_bi_a.big_int_unsigned_sub(test_bi_b);
+    return test_bi_a.big_int_to_string(bi_base::BI_HEX);
+
+}
+*/
--- src/big_int_test_cases/big_int_test_cases_inc/big_int_test_cases.hpp
+++ src/big_int_test_cases/big_int_test_cases_inc/big_int_test_cases.hpp
@@ -14,3 +14,5 @@
 std::string bi_test_big_int_from_string(const std::string &src_str);
 std::string bi_test_big_int_unsigned_add(const std::string &a, const std::string &b);
 std::string bi_test_big_int_unsigned_sub(const std::string &a, const std::string &b);
+std::string bi_test_big_int_unsigned_add_on_obj(const std::string &a, const std::string &b);
+//std::string bi_test_big_int_unsigned_sub_on_obj(const std::string &a, const std::string &b);
