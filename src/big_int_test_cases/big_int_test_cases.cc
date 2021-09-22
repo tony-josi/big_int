580221e878225162b5e9f5bf11a9d847933980d8
TonyJosi97 <tonyjosinew@gmail.com>
2021-09-23 00:24:21 +0530

    pybind api src

--- src/big_int_test_cases/big_int_test_cases.cc
+++ src/big_int_test_cases/big_int_test_cases.cc
@@ -9,4 +9,34 @@
  *  @bug            No known bugs.
  */
 
+#include "big_int_test_cases_inc/big_int_test_cases.hpp"
+#include "../../inc/big_int.hpp"
 
+using namespace bi;
+
+std::string bi_test_big_int_from_hex_string(const std::string &src_str) {
+
+    big_int test_bi;
+    test_bi.big_int_from_string(src_str);
+    return test_bi.big_int_to_string(bi_base::BI_HEX);
+
+}
+
+std::string bi_test_big_int_unsigned_add(const std::string &a, const std::string &b) {
+
+    big_int test_bi_a, test_bi_b, test_bi_c;
+    test_bi_a.big_int_from_string(a);
+    test_bi_b.big_int_from_string(b);
+    test_bi_a.big_int_unsigned_add(test_bi_b, &test_bi_c);
+    return test_bi_c.big_int_to_string(bi_base::BI_HEX);
+
+}
+std::string bi_test_big_int_unsigned_sub(const std::string &a, const std::string &b) {
+
+    big_int test_bi_a, test_bi_b, test_bi_c;
+    test_bi_a.big_int_from_string(a);
+    test_bi_b.big_int_from_string(b);
+    test_bi_a.big_int_unsigned_sub(test_bi_b, &test_bi_c);
+    return test_bi_c.big_int_to_string(bi_base::BI_HEX);
+
+}
