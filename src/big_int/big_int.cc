6ae43c6acdaec4eceec766e6e9f7e953bb9dd15d
TonyJosi97 <tonyjosinew@gmail.com>
2021-10-01 01:35:28 +0530

    cprgt edited

--- src/big_int/big_int.cc
+++ src/big_int/big_int.cc
@@ -2,7 +2,7 @@
  *  @file   big_int.cc
  *  @brief  Main source file for the big int library
  *
- *  This file contains the source code the DS used as the big in library
+ *  This file contains the source code and the DS used in the big int library
  *
  *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
  *  @copyright      Copyright (C) 2021 Tony Josi
